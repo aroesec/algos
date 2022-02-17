f1189cd8621feea730656268ad8fa5d86493da84
aroesec <aroe3sec@gmail.com>
2022-02-16 21:08:45 -0700

    finish h-index

---
+++ h_index.py
@@ -0,0 +1,15 @@
+# O(nlogn) time
+# O(1) space
+
+citation_input = [1,4,1,4,2,1,3,5,6]
+
+def get_h_index(citation_input):
+    citation_input.sort(reverse= True)
+    h_index = 0
+    for index, value in enumerate(citation_input):
+        if value >= index+1:
+            h_index = index+1
+        else:
+            break
+    return h_index
+print(get_h_index(citation_input))
